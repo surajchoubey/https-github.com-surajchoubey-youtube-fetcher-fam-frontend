e5b5d5c908d2e16bfbbed4387a04e706451df13f
surajchoubey <srjchoubey2@gmail.com>
2024-10-04 00:48:02 +0530

    update base-url

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://127.0.0.1:5000/'
+axios.defaults.baseURL = 'https://youtube-fetcher-fam.onrender.com'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
